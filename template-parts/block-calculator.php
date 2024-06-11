95a20b54d32e230ea88dee919f8295ae226a1146
lukaszmosionek <mosioneklukasz@gmail.com>
2024-06-11 18:30:05 +0200

    Create HTML of the calculator

---
+++ template-parts/block-calculator.php
@@ -0,0 +1,35 @@
+<div id="calculator">
+    <div id="calc-screen">
+        <div id="calc-operation"></div>
+        <div id="calc-result"></div>
+    </div>
+    <div id="calc-buttons">
+        
+        <button class="calc-btn" data-action="clear">C</button>
+        <button class="calc-btn" data-action=""></button>
+        <button class="calc-btn" data-action=""></button>
+        <button class="calc-btn" data-action="divide">/</button>
+
+        <button class="calc-btn" data-value="7">7</button>
+        <button class="calc-btn" data-value="8">8</button>
+        <button class="calc-btn" data-value="9">9</button>
+        <button class="calc-btn" data-action="multiply">X</button>
+
+        <button class="calc-btn" data-value="4">4</button>
+        <button class="calc-btn" data-value="5">5</button>
+        <button class="calc-btn" data-value="6">6</button>
+        <button class="calc-btn" data-action="subtract">-</button>
+
+        <button class="calc-btn" data-value="1">1</button>
+        <button class="calc-btn" data-value="2">2</button>
+        <button class="calc-btn" data-value="3">3</button>
+        <button class="calc-btn" data-action="add">+</button>
+
+        <button class="calc-btn" data-action=""></button>
+        <button class="calc-btn" data-value="0">0</button>
+        <button class="calc-btn" data-action=""></button>
+        <button class="calc-btn" data-action="equals" style="background: #6e5948;">=</button>
+        
+    </div>
+    <button id="save-calculation">Save</button>
+</div>
